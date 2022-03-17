563da81e1204de1d361cfa77411efc91e965cc16
Pablo Durán <pablodhe@ext.inditex.com>
2022-03-17 21:54:36 +0100

    init-db.js corrections

--- init-db.js
+++ init-db.js
@@ -22,46 +22,20 @@ db.markdown.insertMany([
 db.page.insertMany([
     {
         "_id": "bd1df5df-d7ef-45de-8f6c-7482354bccc3",
-        "createdAt": {"$date": "2022-03-13T16:31:26.681Z"},
+        "createdAt": "Sun, 13 Mar 2022 16:31:26 GMT",
         "text": "# Title",
         "title": "test",
-        "updatedAt": {"$date": "2022-03-13T16:31:26.681Z"},
+        "updatedAt": "Sun, 13 Mar 2022 16:31:26 GMT",
         "user": "admin",
         "markdown": "0d0e1224-7f9c-4e81-a254-61617b996da1"
     },
     {
         "_id": "4141506e-7c80-4f7b-8976-3ac739b4747f",
-        "createdAt": {"$date": "2022-03-13T16:31:26.681Z"},
+        "createdAt": "Sun, 13 Mar 2022 16:31:26 GMT",
         "text": "# efiuefifehi efhieuf eifuhe ifuhe fiuefh ",
         "title": "test 2",
-        "updatedAt": {"$date": "2022-03-13T16:31:26.681Z"},
+        "updatedAt": "Sun, 13 Mar 2022 16:31:26 GMT",
         "user": "admin",
         "markdown": "0d0e1224-7f9c-4e81-a254-61617b996da1"
-    }
-]);
-
-db.user.insertMany([
-    {
-        "_id": "30bbccfc-b81c-4420-9fcb-b9eaf671b1c8",
-        "createdAt": {"$date": "2022-03-13T13:13:00.919Z"},
-        "email": "admin@admin.com",
-        "name": "admin",
-        "password": {
-            "$binary": "JDJiJDEyJFNKenhjcGltdU1FSkNON0dIVUQzai44dXQ4LmRBemRZdjY5RnhlV1JlcnY0Znh1SldGZEJL",
-            "$type": "0"
-        },
-        "updatedAt": {"$date": "2022-03-13T13:13:00.919Z"},
-        "role": "ADMIN"
-    },
-    {
-        "_id": "f06f9d89-c49b-4e96-9b3e-6c7172a4211a",
-        "createdAt": {"$date": "2022-03-13T13:13:00.919Z"},
-        "email": "random@random.com",
-        "name": "random",
-        "password": {
-            "$binary": "JDJiJDEyJFNKenhjcGltdU1FSkNON0dIVUQzai44dXQ4LmRBemRZdjY5RnhlV1JlcnY0Znh1SldGZEJL",
-            "$type": "0"
-        },
-        "updatedAt": {"$date": "2022-03-13T13:13:00.919Z"}
     }
 ]);
